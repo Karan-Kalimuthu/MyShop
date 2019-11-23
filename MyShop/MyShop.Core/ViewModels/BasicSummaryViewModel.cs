a92724e4f635aaab323ea755838f38db52ab4bbc
Karan Kalimuthu <56226319+Karan-Kalimuthu@users.noreply.github.com>
2019-11-23 21:31:12 +0530

    Finished Basket.

---
+++ MyShop/MyShop.Core/ViewModels/BasicSummaryViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.ViewModels
+{
+    public class BasicSummaryViewModel
+    {
+        public int BasketCount { get; set; }
+        public decimal BasketTotal { get; set; }
+
+        public BasicSummaryViewModel()
+        {
+
+        }
+
+        public BasicSummaryViewModel(int basketCount, decimal basketTotal)
+        {
+            this.BasketCount = basketCount;
+            this.BasketTotal = basketTotal;
+        }
+    }
+}
